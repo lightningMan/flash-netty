6dacf3b5005d51591a00aab951f3f16a043a702e
chao.yu <chao.yu@dianping.com>
2018-08-05 10:49:13 +0800

    init

--- src/main/java/the/flash/bytebuf/ByteBufTest.java
+++ src/main/java/the/flash/bytebuf/ByteBufTest.java
@@ -7,7 +7,7 @@ public class ByteBufTest {
     public static void main(String[] args) {
         ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(9, 100);
 
-        print("allocate ByteBuf", buffer);
+        print("allocate ByteBuf(9, 100)", buffer);
 
         // write 方法改变写指针，写完之后写指针未到 capacity 的时候，buffer 仍然可写
         buffer.writeBytes(new byte[]{1, 2, 3, 4});
@@ -45,8 +45,8 @@ public class ByteBufTest {
 
     private static void print(String action, ByteBuf buffer) {
         System.out.println("after ===========" + action + "============");
-        System.out.println("capacity():" + buffer.capacity());
-        System.out.println("maxCapacity():" + buffer.maxCapacity());
+        System.out.println("capacity(): " + buffer.capacity());
+        System.out.println("maxCapacity(): " + buffer.maxCapacity());
         System.out.println("readerIndex(): " + buffer.readerIndex());
         System.out.println("readableBytes(): " + buffer.readableBytes());
         System.out.println("isReadable(): " + buffer.isReadable());
