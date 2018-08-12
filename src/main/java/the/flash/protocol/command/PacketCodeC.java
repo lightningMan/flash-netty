1b1c993a3e8ccb603d1972ed8c530fee9bb4ef73
chao.yu <chao.yu@dianping.com>
2018-08-13 05:39:24 +0800

    modify magic number

--- src/main/java/the/flash/protocol/command/PacketCodeC.java
+++ src/main/java/the/flash/protocol/command/PacketCodeC.java
@@ -12,7 +12,7 @@ import static the.flash.protocol.command.Command.LOGIN_REQUEST;
 
 public class PacketCodeC {
 
-    private static final int MAGIC_NUMBER = 0x1234;
+    private static final int MAGIC_NUMBER = 0x12345678;
     private static final Map<Byte, Class<? extends Packet>> packetTypeMap;
     private static final Map<Byte, Serializer> serializerMap;
 
