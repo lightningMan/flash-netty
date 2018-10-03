c0d6dd1c4879a95c707d8d279585aa506b30e2b7
俞超 <yuchao@yuchaodeiMac.local>
2018-10-03 12:27:27 +0800

    init

---
+++ src/main/java/the/flash/server/handler/LifeCyCleTestHandler.java
@@ -0,0 +1,55 @@
+package the.flash.server.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class LifeCyCleTestHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("逻辑处理器被添加：handlerAdded()");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel 绑定到线程(NioEventLoop)：channelRegistered()");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel 准备就绪：channelActive()");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("channel 有数据可读：channelRead()");
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel 某次数据读完：channelReadComplete()");
+        super.channelReadComplete(ctx);
+
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel 被关闭：channelInactive()");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel 取消线程(NioEventLoop) 的绑定: channelUnregistered()");
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("逻辑处理器被移除：handlerRemoved()");
+        super.handlerRemoved(ctx);
+    }
+}
